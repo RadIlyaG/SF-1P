ce3416b91d2c751029046aad0e1655d59df543d0
Ilya Ginzburg <ilya_g@rad.com>
2026-01-05 11:24:49 +0200

    Update HWinit of AT-2

--- AT-SF-1P/software/at-sf1p-2-w10/HWinit.tcl
+++ AT-SF-1P/software/at-sf1p-2-w10/HWinit.tcl
@@ -2,10 +2,10 @@
 switch -exact -- $gaSet(pair) {
   1 - 5 - SE {
       #set gaSet(comDut)     7; #1
-      set gaSet(comGen1)    5
-      set gaSet(comGen2)    6
+      set gaSet(comGen1)    5; # 5
+      set gaSet(comGen2)    6; # 6
       set gaSet(comDut) [set gaSet(comSer2) 4]
-      set gaSet(comSer1)    2
+      set gaSet(comSer1)    2; # 2
       set gaSet(comSer485)  13; #2
       console eval {wm geometry . +150+1}
       console eval {wm title . "Con 1"} 
@@ -14,10 +14,10 @@ switch -exact -- $gaSet(pair) {
   }
   2 {
       #set gaSet(comDut)     1; #6
-      set gaSet(comGen1)    10
-      set gaSet(comGen2)    9
+      set gaSet(comGen1)    9; # 10
+      set gaSet(comGen2)    7; # 9
       set gaSet(comDut) [set gaSet(comSer2) 8]
-      set gaSet(comSer1)    7
+      set gaSet(comSer1)    10; # 7
       set gaSet(comSer485)  14; #9  
       console eval {wm geometry . +150+200}
       console eval {wm title . "Con 2"} 
